ef865c43b031cd722b4d600cf205be20e0203d15
Rerum <test@example.com>
2016-12-20 07:04:14 +0300

    Added tests

---
+++ tests/test1.pas
@@ -0,0 +1,17 @@
+Var a,b,c,sum, var;
+Begin
+	a := 0;
+	b := 1;
+	c := 999;
+	sum := 0;
+	/*
+	sum := 5;
+	*/
+
+	sum := not a / c + b;
+	a := 50;
+	REPEAT
+		a := a + 9;
+		b := b + 10;
+	UNTIL b > a
+End
